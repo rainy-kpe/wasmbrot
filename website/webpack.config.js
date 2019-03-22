9adc17ac39d78149b68e393e2dc10b0c1cea8797
Kimmo Pekkola <kimmo.pekkola@gmail.com>
2019-03-22 13:40:49 +0200

    Added typescript support for the website

--- website/webpack.config.js
+++ website/webpack.config.js
@@ -1,14 +1,27 @@
 const CopyWebpackPlugin = require("copy-webpack-plugin");
-const path = require('path');
+const path = require("path");
 
-module.exports = {
-  entry: "./bootstrap.js",
+module.exports = env => ({
+  entry: "./src/bootstrap.js",
+  mode: env === "prod" ? "production" : "development",
+  devtool: env === "prod" ? "none" : "inline-source-map",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bootstrap.js",
   },
-  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [ ".tsx", ".ts", ".js", ".wasm" ]
+  },
   plugins: [
-    new CopyWebpackPlugin(['index.html'])
+    new CopyWebpackPlugin([{ from: "public/*", flatten: true }])
   ],
-};
+});
